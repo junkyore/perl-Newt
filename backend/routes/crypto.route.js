e0e9d847006cf73de3f38e646fc183902aed79b5
junkyore <106412691+junkyore@users.noreply.github.com>
2020-12-13 19:53:13 +0000

    solved

--- backend/routes/crypto.route.js
+++ backend/routes/crypto.route.js
@@ -1,1 +1,25 @@
-const express
+const express = require('express');
+const router = express.Router();
+const Crypto = require('../models/Crypto');
+
+router.get('/api', async (req, res) => {
+	try {
+		const data = await Crypto.find();
+		res.json(data);
+	} catch (e) {
+		console.log(e);
+	}
+});
+router.post('/add-crypto', async (req, res) => {
+	try {
+		let crypto = new Crypto(req.body);
+		crypto = await crypto.save();
+	} catch (e) {
+		console.log(e);
+	}
+});
+
+router.delete('/delete-crypto/:id', async (req, res) => {
+	await Crypto.findByIdAndDelete(req.params.id);
+});
+module.exports = router;

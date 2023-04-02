648833241a059fb536605019a5a475ae7620b012
junkyore <106412691+junkyore@users.noreply.github.com>
2023-04-02 09:13:02 +0000

    bump version to 0.2.1

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,35 @@
 <template>
 	<div class="container">
 		<div class="logo">
-			<a href="#"
+			<a href="#"><img src="@/assets/Logo.svg" alt=""/></a>
+		</div>
+		<Form @clicked="handleSubmitForm" />
+		<div class="crypto-container">
+			<Crypts
+				v-for="(cryps, name) in CryptoList"
+				:key="cryps._id"
+				:cryps="cryps"
+				:name="name"
+			/>
+		</div>
+	</div>
+</template>
+
+<script>
+import Form from '@/components/form';
+import Crypts from '@/components/Crypts';
+import { mapActions, mapState } from 'vuex';
+import axios from 'axios';
+export default {
+	name: 'App',
+	data() {},
+	components: { Form, Crypts },
+	created() {
+		this.getCryptos();
+	},
+	methods: {
+		...mapActions(['getCryptos']),
+		async handleSubmitForm(data) {
+			await axios
+				.post('http://localhost:9000/add-crypto', { name: data })
+				.th

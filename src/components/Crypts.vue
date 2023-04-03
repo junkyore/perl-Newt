fb43df9013a1932c1e1cac1763e3310d8816ec27
junkyore <106412691+junkyore@users.noreply.github.com>
2023-04-03 20:02:17 +0000

    added tasks

--- src/components/Crypts.vue
+++ src/components/Crypts.vue
@@ -1,2 +1,44 @@
 <template>
-	<div cl
+	<div class="crypts">
+		<p class="price">${{ cryps.USD }}</p>
+		<div class="lower">
+			<p class="crypto-label">{{ name }}</p>
+			<button class="delete-btn"><i class="far fa-trash-alt"></i></button>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	props: ['cryps', 'name'],
+};
+</script>
+
+<style lang="scss" scoped>
+.crypts {
+	background: #32373c;
+	border-radius: 1em;
+}
+.price {
+	font-size: 2.5em;
+	font-weight: 200;
+	padding: 1em 0;
+}
+.lower {
+	background: #292d30;
+	padding: 1em;
+	position: relative;
+	height: 100px;
+	border-radius: 1em;
+}
+.crypto-label {
+	text-transform: uppercase;
+	font-size: 1.5em;
+	font-weight: bold;
+	margin-top: -1.5em;
+	color: #788591;
+}
+.delete-btn {
+	background: none;
+	border: none;
+	position: absolute;
